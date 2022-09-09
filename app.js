ae539f15c35dcc1f1039d3a793b60ced95a5190e
Gabriel <gajmarin@bancolombia.com.co>
2022-09-08 20:09:49 -0500

    algoritmo con sockets

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const session = require("express-session");
+const fs = require('fs');
 
 const app = express();
 
@@ -14,16 +15,36 @@ const io = new Server(httpServer, {});
 
 io.on("connection", (socket) => {
     console.log(`El cliente ${socket.id} se ha CONECTADO`);
-    
-    socket.on('client:sendData', (data)=>{
-        console.log(data);
+
+    socket.on('client:sendData', (data) => {
+        fs.writeFile('datos.txt', data.cuenta + ',' + data.saldo + '\n', { flag: 'a' }, (err) => {
+            socket.emit('server:sendData', { error: false, mensaje: 'La cuenta '+data.cuenta+' se ha guardado de manera exitosa!!!' })
+            if (err) {
+                socket.emit('server:sendData', { error: true, mensaje: 'No se pudo guardar la cuenta '+data.cuenta })
+            }
+        });
     })
 
-    socket.on('disconnect', function() {
+
+    socket.on('client:findData', (mensaje) => {
+        console.log(mensaje);
+        fs.readFile('datos.txt', 'utf8', (err, file) => {
+            const datos = file.split('\n')
+                .map(x => ({ "cuenta": x.slice(0, x.indexOf(',')), "saldo": x.slice(x.indexOf(',') + 1) }))
+                .filter(x => x.cuenta == mensaje.cuenta)
+            let respuesta = { error: true, data:[] }
+            if (datos.length > 0) {
+                respuesta.error = false
+                respuesta.data = datos[0]
+            }
+            socket.emit('server:findData', respuesta)
+        })
+    })
+
+
+    socket.on('disconnect', function () {
         console.log(`El cliente ${socket.id} se ha DESCONECTADO`);
-  
-       
-     });
+    });
 });
 
 
